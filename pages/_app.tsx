d546e97317e712fb1283d10a97cc805cb373d4a5
Paul Olajide <paulolajide.dev@gmail.com>
2022-07-16 11:18:37 +0100

    refactor: 💡 fonts addition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import { customTheme } from "../styles/themes";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { customTheme } from '../styles/themes'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={customTheme}>
       <Component {...pageProps} />
     </ChakraProvider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
