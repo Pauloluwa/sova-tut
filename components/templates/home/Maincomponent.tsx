e40b369befa0d1d6143b1bf4116f2b97e926237a
Paul Olajide <paulolajide.dev@gmail.com>
2022-08-30 22:27:15 +0100

    feat: 🎸 Hero page

--- components/templates/home/Maincomponent.tsx
+++ components/templates/home/Maincomponent.tsx
@@ -1,20 +1,20 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Heading, Image, Img } from '@chakra-ui/react'
 import React from 'react'
 
 const Maincomponent = () => {
   return (
     <Box h={'10rem'} w="100%" bg={'white'} padding="0">
-      <div>
-        <div>
-          <img src="/images/logo.png" alt="logo" />
-          <img src="/images/logo.png" alt="logo" />
-          <img src="/images/logo.png" alt="logo" />
-        </div>
-        <div>
-          <img src="/images/logo.png" alt="logo" />
-          <h1>Litkovska Collection</h1>
-        </div>
-      </div>
+      <Box>
+        <Box>
+          <Img src="/images/logo.png" alt="logo" />
+          <Img src="/images/logo.png" alt="logo" />
+          <Img src="/images/logo.png" alt="logo" />
+        </Box>
+        <Box>
+          <Image src="/images/logo.png" alt="logo" />
+          <Heading>Litkovska Collection</Heading>
+        </Box>
+      </Box>
     </Box>
   )
 }
--- components/templates/home/Navbar.tsx
+++ components/templates/home/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Container, HStack, Icon } from '@chakra-ui/react'
+import { Box, Center, Container, HStack, Icon, Img } from '@chakra-ui/react'
 import { FiHeart, FiLogOut, FiSearch } from 'react-icons/fi'
 import { BsPerson } from 'react-icons/bs'
 
@@ -8,7 +8,7 @@ const Navbar = () => {
       <Container maxW={'1204px'} h={'full'} w={'full'}>
         <HStack w={'full'} spacing={'1rem'} h={'full'}>
           <Box w="25%">
-            <img src="/images/logo.png" alt="logo" />
+            <Img src="/images/logo.png" alt="logo" />
           </Box>
           <HStack spacing={'1rem'} w={'50%'} h={'full'} justify={'center'}>
             {['jewelry', 'Collection', 'Outlet', 'Our Production'].map((txt, idx) => (
